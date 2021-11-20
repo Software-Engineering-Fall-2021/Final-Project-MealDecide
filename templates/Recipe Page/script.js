9ba489d7af3b5a3c20d950097414ade2cae1bda9
God-Speed4 <cortezlbanks@lewisu.edu>
2021-11-20 01:27:10 -0600

    adding recipe page

---
+++ templates/Recipe Page/script.js
@@ -0,0 +1,38 @@
+const searchForm = document.querySelector('form');
+const searchResultDiv = document.querySelector('.search-result');
+const container = document.querySelector('.container');
+const Type="public";
+const APP_ID = '772bcae9';
+const APP_KEY = '8d3d0e383805de9a7ce3178e5d0605df';
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchQuery = e.target.querySelector('select','input','option').value;
+    fetchAPI();
+});
+// An asynchronous function to fetch dta from the API.
+async function fetchAPI(){
+    const baseURL = `https://api.edamam.com/api/recipes/v2?type=${Type}&q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&to=20`
+    const response = await fetch (baseURL);
+    console. log(response);
+    const data = await response.json();
+    genrateHTML(data.hits);
+    console. log(data)
+    useApiData(data)
+}
+function genrateHTML(results){
+    let genreatedHTML = '';
+    results.map(results =>{
+        genreatedHTML +=
+    
+    `<div class="item">
+    <image src="${results.recipe.image}" alt+"">
+        <div class="flex-container">
+            <h1 class="title">${results.recipe.label}</h1>
+            <a class="view-button" href="${results.recipe.url}"target="_blank">View Recipe</a>
+        </div>
+        <p class="item-data">Calories: ${results.recipe.calories.toFixed(2)}</p>
+        </div>`
+    })
+    searchResultDiv.innerHTML = genreatedHTML;
+}
